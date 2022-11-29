c1ee04f1cf16de681001489ae60d62db31d994b6
Angel <santoyox714@gmail.com>
2022-11-29 01:11:42 -0800

    init react/pocketbase

---
+++ src/app.jsx
@@ -0,0 +1,23 @@
+const USERNAME = 'santoyox714@gmail.com'
+const PASSWORD = 'devTest1234'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
+console.log(authData)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>Hello, {authData.admin.email}</h1>
+    </div>
+  )
+}
